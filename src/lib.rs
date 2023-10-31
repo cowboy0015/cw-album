fd845df097d2ee58d4589f91e46eb4e85a1e981e
cowboy0015 <web3pirate0105@otulook.com>
2023-10-31 08:57:38 -0500

    add: added test case for nft transfer & readme file

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use cosmwasm_schema::cw_serde;
 use cosmwasm_std::Empty;
 pub use cw721_base::{ContractError, InstantiateMsg, MinterResponse};
 
+
 #[cw_serde]
 #[derive(Default)]
 pub struct Metadata {
@@ -63,10 +64,19 @@ pub mod entry {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use cosmwasm_std::{CosmosMsg, WasmMsg, Response, to_binary, Uint128};
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cw721::Cw721Query;
+    use cw721::{Cw721Query, Cw721ReceiveMsg};
+    use cw_ownable::OwnershipError;
 
     const CREATOR: &str = "creator";
+    #[cw_serde]
+    struct AuctionConfig {
+        start_time: u64,
+        duration: u64,
+        coin_denom: String,
+        min_bid: Option<Uint128>,
+    }
 
     #[test]
     fn cw721_album_metadata()
@@ -104,5 +114,91 @@ mod tests {
         let res = contract.nft_info(deps.as_ref(), token_id.into()).unwrap();
         assert_eq!(res.token_uri, token_uri);
         assert_eq!(res.extension, extension);
+    }
+    #[test]
+    fn test_receive_nft_msg() {
+        let mut deps = mock_dependencies();
+        let contract = Cw721MetadataContract::default();
+        let init_msg = InstantiateMsg {
+            name: "cw721-album".to_string(),
+            symbol: "cw_foo".to_string(),
+            minter: CREATOR.to_string(),
+        };
+        let info = mock_info(CREATOR, &[]);
+        contract
+            .instantiate(deps.as_mut(), mock_env(), info.clone(), init_msg)
+            .unwrap();
+
+        // Mint a token
+        let token_id = "pirate".to_string();
+        let token_uri = Some("https://pirate.com/evil".into());
+        let extension = Some(Metadata {
+            artist: "Albert".into(),
+            album: "https://pirate.album/best-sellers".to_string(),
+            ..Metadata::default()
+        });
+        let exec_msg = ExecuteMsg::Mint {
+            token_id: token_id.clone(),
+            owner: "albert".to_string(),
+            token_uri: token_uri.clone(),
+            extension: extension.clone(),
+        };
+
+        let minter = mock_info(CREATOR, &[]);
+        contract
+            .execute(deps.as_mut(), mock_env(), minter, exec_msg)
+            .unwrap();
+
+
+        let msg = to_binary(
+                &AuctionConfig {
+                    start_time: 0,
+                    duration: 1000000,
+                    coin_denom: "usd".to_string(),
+                    min_bid: Some(Uint128::from(10000u128)),
+                }
+            ).unwrap();
+        let target = String::from("another_contract");
+        let send_msg = ExecuteMsg::SendNft {
+            contract: target.clone(),
+            token_id: token_id.clone(),
+            msg: msg.clone(),
+        };
+
+        let random = mock_info("random", &[]);
+        let err = contract
+            .execute(deps.as_mut(), mock_env(), random, send_msg.clone())
+            .unwrap_err();
+        assert_eq!(err, ContractError::Ownership(OwnershipError::NotOwner));
+
+        // but owner can
+        let random = mock_info("albert", &[]);
+        let res = contract
+            .execute(deps.as_mut(), mock_env(), random, send_msg)
+            .unwrap();
+
+        let payload = Cw721ReceiveMsg {
+            sender: String::from("albert"),
+            token_id: token_id.clone(),
+            msg,
+        };
+        let expected = payload.into_cosmos_msg(target.clone()).unwrap();
+        // ensure expected serializes as we think it should
+        match &expected {
+            CosmosMsg::Wasm(WasmMsg::Execute { contract_addr, .. }) => {
+                assert_eq!(contract_addr, &target)
+            }
+            m => panic!("Unexpected message type: {m:?}"),
+        }
+        // and make sure this is the request sent by the contract
+        assert_eq!(
+            res,
+            Response::new()
+                .add_message(expected)
+                .add_attribute("action", "send_nft")
+                .add_attribute("sender", "albert")
+                .add_attribute("recipient", "another_contract")
+                .add_attribute("token_id", token_id)
+        );
     }
 }
